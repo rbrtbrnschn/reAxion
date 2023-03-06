aa8b7cb7dfc8d5ce4797928697b4145f36871070
Robert Bornschein <robertbornscheinofficial@gmail.com>
2023-03-06 17:23:32 +0100

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { GameScreen } from "./screens/game/game.screen";
 import { HomeScreen } from "./screens/home/home.screen";
 
 export const App = () => {
+  console.log("Nice.");
   return <HomeScreen />;
 };
